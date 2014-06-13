929910a15b918326fb423348fba4325d07d6d504
onphenomenon <kariannechristina@gmail.com>
2014-06-12 20:49:25 -0700

    Letter Frequency HASH

--- coding-test-2/practice-problems/lib/02_letter_count.rb
+++ coding-test-2/practice-problems/lib/02_letter_count.rb
@@ -1,2 +1,17 @@
+
 def letter_count(str)
+	letter_hash = {}
+	str = str.delete(' ')
+	n = 0
+	while n < str.length
+		if letter_hash[str[n]] == 1
+			letter_hash[str[n]] = letter_hash[str[n]] + 1
+		end
+		if letter_hash[str[n]] == nil
+		 	letter_hash[str[n]] = 1
+		end
+		n += 1
+		
+	end
+	return letter_hash
 end
